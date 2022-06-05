e33b67dbd5f54f9e1dccaec8ee6287d626d47343
Armanc Keser <armanckeser@hotmail.com>
2022-06-05 01:47:36 -0400

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using Harmonylib;
+
+namespace TrackCrafts;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("xyz.molenzwiebel.wetstone")]
+[Wetstone.API.Reloadable]
+public class Plugin : BasePlugin
+{
+    /// <summary>
+    ///     Most of the variables we need for the plugin.
+    /// </summary>
+    public static ManualLogSource Logger;
+
+    public static ConfigEntry<bool> Enabled;
+    private Harmony _harmony;
+    public override void Load()
+    {
+        Logger = Log;
+        _harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        _harmony.PatchAll(Assembly.GetExecutingAssembly());
+        Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+}
