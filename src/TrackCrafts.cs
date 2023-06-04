b5543f8d6adf7fff0808dbf83aa2721677012212
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-06-04 17:16:47 +1000

    Updated for Gloom

--- src/TrackCrafts.cs
+++ src/TrackCrafts.cs
@@ -1,11 +1,10 @@
 using HarmonyLib;
 using System;
+using Bloodstone.API;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using ProjectM;
 using ProjectM.UI;
-using Wetstone.API;
 
 namespace TrackCrafts;
 
@@ -13,10 +12,14 @@ namespace TrackCrafts;
 public class TrackCrafts : MonoBehaviour
 {
     public static TrackCrafts Instance;
-    private static GameObject recipesParent;
-    private static GameObject layout;
-    private Keybinding ClearPinnedKeybind;
-    private Keybinding HidePinnedKeybind;
+    private static GameObject RecipesParent { get; set; }
+    private static GameObject Layout { get; set; }
+
+    private static VerticalLayoutGroup RecipesParentLayout { get; set; }
+    private static ContentSizeFitter RecipesParentSizeFitter { get; set; }
+
+    private Keybinding ClearPinnedKeybind { get; set; }
+    private Keybinding HidePinnedKeybind { get; set; }
 
     private void Awake()
     {
@@ -43,100 +46,153 @@ public class TrackCrafts : MonoBehaviour
     {
         if (Input.GetKeyDown(ClearPinnedKeybind.Primary) || Input.GetKeyDown(ClearPinnedKeybind.Secondary))
         {
-            destroyRecipes();
+            DestroyRecipes();
         }
 
         if (Input.GetKeyDown(HidePinnedKeybind.Primary) || Input.GetKeyDown(HidePinnedKeybind.Secondary))
         {
-            hideRecipes();
+            HideRecipes();
         }
     }
 
     [HarmonyPatch(typeof(GridSelectionEntry), nameof(GridSelectionEntry.OnPointerClick))]
     [HarmonyPostfix]
-    static void OnPointerClick(GridSelectionEntry __instance, PointerEventData eventData)
+    private static void OnPointerClick(GridSelectionEntry __instance, PointerEventData eventData)
     {
-        if (layout == null)
-            layout = GameObject.Find("HUDCanvas(Clone)/Canvas/HUDAchievements/JournalParent(Clone)/Layout");
-        if (recipesParent == null)
+        //this path changed
+        Layout ??= GameObject.Find("HUDCanvas(Clone)/JournalCanvas/JournalParent(Clone)/Layout");
+        RecipesParent ??= new GameObject("Recipes Parent")
         {
-            recipesParent = new GameObject("Recipes Parent");
-            recipesParent.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
-            recipesParent.transform.parent = layout.transform;
-            VerticalLayoutGroup recipesParentLayout = recipesParent.AddComponent<VerticalLayoutGroup>();
-            recipesParentLayout.childControlHeight = true;
-            recipesParentLayout.childControlWidth = true;
-            recipesParentLayout.spacing = 20;
-            recipesParent.AddComponent<ContentSizeFitter>();
-        }
+            transform =
+            {
+                localScale = new Vector3(Plugin.TrackerItemScale.Value, Plugin.TrackerItemScale.Value, 1f),
+                parent = Layout.transform
+            }
+        };
+        
+        RecipesParentLayout ??= RecipesParent.AddComponent<VerticalLayoutGroup>();
+        RecipesParentLayout.childControlHeight = true;
+        RecipesParentLayout.childControlWidth = true;
+        RecipesParentLayout.spacing = 20;
+
+        RecipesParentSizeFitter ??= RecipesParent.AddComponent<ContentSizeFitter>();
+
         var entry = __instance.GetComponent<WorkstationRecipeGridSelectionEntry>();
         if (entry == null)
             return;
-        if (eventData.button == PointerEventData.InputButton.Middle)
+        if (eventData.button != PointerEventData.InputButton.Middle) return;
+        if (RecipesParent.transform.GetChildCount() < Plugin.TrackQuantity.Value)
+            PinRecipe(__instance);
+    }
+
+    private static void HideRecipes()
+    {
+        if (RecipesParent != null)
         {
-            if (recipesParent.transform.GetChildCount() < 3)
-                pinRecipe();
+            RecipesParent.active = !RecipesParent.active;
         }
     }
 
-    private static void hideRecipes()
+    private static void DestroyRecipes()
     {
-        if (recipesParent != null)
-        {
-            recipesParent.active = !recipesParent.active;
-        }
-    }
-    private static void destroyRecipes()
-    {
-        if (recipesParent != null)
-        {
-            foreach (Transform child in recipesParent.transform.GetAllChildren())
-            {
-                UnityEngine.Object.Destroy(child.gameObject);
-            }
-            UnityEngine.Object.Destroy(recipesParent);
-            recipesParent = null;
-        }
-    }
-
-    private static void pinRecipe()
-    {
-        GameObject currentTooltip = GameObject.Find("HUDCanvas(Clone)/Canvas/HUDMenuParent/WorkstationMenu(Clone)/MenuParent/WorkstationSubMenu(Clone)/MotionRoot/FakeTooltip");
-        if (currentTooltip == null)
-            return;
-        createAndAttachFakeToolTip(currentTooltip, recipesParent);
+        //no need to destroy children. When you destroy the parent, the children are destroyed as well
+        if (RecipesParent == null) return;
+        Destroy(RecipesParent);
+        RecipesParent = null;
     }
 
 
-    private static void createAndAttachFakeToolTip(GameObject currentTooltip, GameObject parent)
+    private static void PinRecipe(GridSelectionEntry gridSelectionEntry)
     {
-        GameObject tooltip = Instantiate(currentTooltip, parent.transform);
-        VerticalLayoutGroup layoutGroup = tooltip.GetComponent<VerticalLayoutGroup>();
+        var currentTooltip = GameObject.Find("HUDCanvas(Clone)/Canvas/HUDMenuParent/WorkstationMenu(Clone)/MenuParent/WorkstationSubMenu(Clone)/MotionRoot/FakeTooltip");
+        if (currentTooltip == null) return;
+        CreateAndAttachFakeToolTip(currentTooltip, RecipesParent, gridSelectionEntry);
+    }
+
+
+    private static void CreateAndAttachFakeToolTip(GameObject currentTooltip, GameObject parent, GridSelectionEntry gridSelectionEntry)
+    {
+        var tooltip = Instantiate(currentTooltip, parent.transform);
+        var layoutGroup = tooltip.GetComponent<VerticalLayoutGroup>();
+        
         layoutGroup.childControlHeight = true;
-        tooltip.transform.GetChild(2).gameObject.active = false;
-        GameObject entries = tooltip.transform.FindChild("Entries").gameObject;
-        deactivateExtraText(entries);
+        if (tooltip.transform.childCount < Plugin.TrackQuantity.Value) return;
+        tooltip.transform.GetChild(3).gameObject.SetActive(false);
+        //this transform name changed
+        var entries = tooltip.transform.Find("InformationEntries");
+
+        var entriesGameObject = entries.gameObject;
+        DeactivateExtraText(entriesGameObject);
         layoutGroup.OnTransformChildrenChanged();
+
         tooltip.AddComponent<GraphicRaycaster>();
-        GameObject sampleButton = GameObject.Find("HUDCanvas(Clone)/Canvas/HUDMenuParent/WorkstationMenu(Clone)/MenuParent/CharacterInventorySubMenu2(Clone)/MotionRoot/SharedForNowAtLeast/SortButton");
-        GameObject buttonParent = tooltip;
-        GameObject deleteButtonObject = Instantiate(sampleButton, buttonParent.transform);
+        var sampleButton = GameObject.Find("HUDCanvas(Clone)/Canvas/HUDMenuParent/WorkstationMenu(Clone)/MenuParent/CharacterInventorySubMenu2(Clone)/MotionRoot/SharedForNowAtLeast/SortButton");
+
+        var deleteButtonObject = Instantiate(sampleButton, tooltip.transform);
+
         deleteButtonObject.name = "Delete Button";
-        deleteButtonObject.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = "X";
-        SimpleStunButton button = deleteButtonObject.GetComponent<ProjectM.UI.SimpleStunButton>();
-        var deleteOnClick = () => UnityEngine.Object.Destroy(tooltip);
-        button.onClick.AddListener(new Action(deleteOnClick));
-        tooltip.active = true;
-    }
-    private static void deactivateExtraText(GameObject entries)
-    {
-        entries.transform.FindChild("Stats").gameObject.active = false;
-        entries.transform.FindChild("TooltipDurability").gameObject.active = false;
-        entries.transform.FindChild("TooltipDesc").gameObject.active = false;
+        var textComponent = deleteButtonObject.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>();
+
+
+        textComponent.text = "X";
+        var button = deleteButtonObject.GetComponent<SimpleStunButton>();
+
+        void DeleteOnClick() => Destroy(tooltip);
+        button.onClick.AddListener(new Action(DeleteOnClick));
+
+        tooltip.SetActive(true);
+        tooltip.name = $"TrackCrafts_{gridSelectionEntry.name}";
     }
 
-    public static void Reset()
+    private static Transform FindTransform(Transform parent, string name)
     {
-        destroyRecipes();
+        if (parent == null)
+        {
+            return null;
+        }
+
+        if (parent.name == name)
+        {
+            return parent;
+        }
+
+        foreach (var o in parent)
+        {
+            var child = o.TryCast<Transform>();
+            var result = FindTransform(child, name);
+            if (result != null)
+            {
+                return result;
+            }
+        }
+
+        return null;
+    }
+
+
+    private static void DeactivateExtraText(GameObject entries)
+    {
+        var levelIcon = FindTransform(entries.gameObject.transform.parent, "LevelFrame");
+        if (levelIcon != null)
+        {
+            levelIcon.gameObject.SetActive(false);
+        }
+
+        foreach (var o in entries.transform)
+        {
+            var child = o.TryCast<Transform>();
+            if (child == null) continue;
+            var childGameObject = child.gameObject;
+            if (childGameObject == null) continue;
+            if (!child.name.Equals("Tooltip_RecipeRequiredItems"))
+            {
+                childGameObject.SetActive(false);
+            }
+        }
+    }
+
+    public static void ResetAll()
+    {
+        DestroyRecipes();
     }
 }
